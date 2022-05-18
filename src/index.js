42eda17fb5fe5cff8d00783f8f29f73aeba9be80
Stefano Corra <stefano.corra@hotmail.com>
2022-05-18 10:27:16 -0500

    fixing issues

--- src/index.js
+++ src/index.js
@@ -2,26 +2,27 @@ import React from 'react'
 import App from './App'
 import './css/main.css'
 import Details from './components/Details'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('app')
-const root = createRoot(container)
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <Router>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/details/:post_id' element={<Details />} />
-      <Route
-        path='*'
-        element={
-          <main style={{ padding: '1rem' }}>
-            <p>There's nothing here!</p>
-          </main>
-        }
-      />
-      <Route />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/details/:post_id' element={<Details />} />
+        <Route
+          path='*'
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+        <Route />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
