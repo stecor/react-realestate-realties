c5bcfb1e895d91064ecf5dc970d83264b2a4970c
Stefano Corra <stefano.corra@hotmail.com>
2022-05-17 22:58:45 -0500

    yarn.lock

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
 import './css/main.css'
 import Details from './components/Details'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/details/:post_id' element={<Details />} />
-        <Route
-          path='*'
-          element={
-            <main style={{ padding: '1rem' }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-        <Route />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+root.render(
+  <Router>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/details/:post_id' element={<Details />} />
+      <Route
+        path='*'
+        element={
+          <main style={{ padding: '1rem' }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+      <Route />
+    </Routes>
+  </Router>
 )
